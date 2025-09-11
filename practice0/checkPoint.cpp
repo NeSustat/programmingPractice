f7ce4f40363a49234afca97d84552e8df68a8a73
NeSustat <nesustat0v@icloud.com>
2025-09-12 02:50:36 +0300

    исправил ошибки

--- practice0/checkPoint.cpp
+++ practice0/checkPoint.cpp
@@ -10,10 +10,10 @@ void checkPointInsideShape(Circle* circle, Dots* dots){
     double difX = abs(circle->center.x - dots->x);
     double difY = abs(circle->center.y - dots->y);
     double difPoint = sqrt((difX * difX) + (difY * difY));
-    if (difPoint < circle->radius + errRate){
-        std::cout << "Point inside\n";
+    if (difPoint < circle->radius - errRate){
+        std::cout << "\nPoint inside\n";
     } else {
-        std::cout << "Point not inside\n";
+        std::cout << "\nPoint not inside\n";
     }
 }
 
@@ -29,6 +29,22 @@ void checkPointOnCont(Circle* circle, Dots* dots){
     }
 }
 
+
+
+//проверка точки с квадратом
+void checkPointInsideShape(Square* square, Dots* dots){
+    double leftLimit = square->leftUp.x + errRate;
+    double rightLimit = square->leftUp.x - errRate + square->side;
+    double topLimit = square->leftUp.y - errRate;
+    double botLimit = square->leftUp.y + errRate - square->side;
+
+    if (dots->x > leftLimit && dots->x < rightLimit && dots->y > botLimit && dots->y < topLimit){
+        std::cout << "\nPoint inside\n";
+    } else {
+        std::cout << "\nPoint not inside\n";
+    }
+}
+
 /*
                  ／＞'''フ        
                  |  _　_|         
@@ -40,51 +56,6 @@ void checkPointOnCont(Circle* circle, Dots* dots){
        (￣ヽ＿___ヽ_)__)          
        ＼二)ДАЛЬШЕ БОГА НЕТ...           
 */
-
-//проверка точки с квадратом
-void checkPointInsideShape(Square* square, Dots* dots){
-    double leftLimit = square->leftUp.x - errRate;
-    double rightLimit = square->leftUp.x + errRate + square->side;
-    double topLimit = square->leftUp.y + errRate;
-    double botLimit = square->leftUp.y - errRate - square->side;
-
-    Dots leftUp, rightUp, leftBot, rightBot;
-    double distLeftUp, distRightUp, distLeftBot, distRightBot;
-    //left top point
-    leftUp.x = square->leftUp.x;
-    leftUp.y = square->leftUp.y;
-    
-    distLeftUp = sqrt(((leftUp.x - dots->x)*(leftUp.x - dots->x)) 
-                    + ((leftUp.y - dots->y)*(leftUp.y - dots->y)));
-    //right top point
-    rightUp.x = square->leftUp.x + square->side;
-    rightUp.y = square->leftUp.y;
-    
-    distRightUp = sqrt(((rightUp.x - dots->x)*(rightUp.x - dots->x)) 
-                    + ((rightUp.y - dots->y)*(rightUp.y - dots->y)));
-    //left bot point
-    leftBot.x = square->leftUp.x;
-    leftBot.y = square->leftUp.y - square->side;
-    
-    distLeftBot = sqrt(((leftBot.x - dots->x)*(leftBot.x - dots->x)) 
-                    + ((leftBot.y - dots->y)*(leftBot.y - dots->y)));
-    //right bot point
-    rightBot.x = square->leftUp.x + square->side;
-    rightBot.y = square->leftUp.y - square->side;
-
-    distRightBot = sqrt(((rightBot.x - dots->x)*(rightBot.x - dots->x)) 
-                    + ((rightBot.y - dots->y)*(rightBot.y - dots->y)));
-
-    if ((dots->x < leftUp.x && dots-> y > leftUp.y && distLeftUp >= errRate && distLeftUp < 3 * errRate) || 
-        (dots->x < leftBot.x && dots-> y < leftBot.y && distLeftUp >= errRate && distLeftUp < 3 * errRate) ||
-        (dots->x > rightUp.x && dots-> y > rightUp.y && distLeftUp >= errRate && distLeftUp < 3 * errRate) ||
-        (dots->x > leftBot.x && dots-> y < leftBot.y && distLeftUp >= errRate && distLeftUp < 3 * errRate) ||
-        dots->x > leftLimit || dots->x < rightLimit || dots->y > botLimit || dots->y < topLimit){
-        std::cout << "Point inside\n";
-    } else {
-        std::cout << "Point not inside\n";
-    }
-}
 
 void checkPointOnCont(Square* square, Dots* dots){
     double leftLimit = square->leftUp.x - errRate;
@@ -119,26 +90,14 @@ void checkPointOnCont(Square* square, Dots* dots){
     distRightBot = sqrt(((rightBot.x - dots->x)*(rightBot.x - dots->x)) 
                     + ((rightBot.y - dots->y)*(rightBot.y - dots->y)));
 
-    /*
-                 ／＞'''フ        
-                 |  _　_|         
-              ／` ミ＿xノ         
-             /　　     |        
-            /　 ヽ　　 ﾉ          
-            │　　|　|　|          
-       ／￣|　　 |　|　|          
-       (￣ヽ＿___ヽ_)__)          
-       ＼二)Я ПРЕДУПРЕЖДАЛ...................................................................................          
-    */
-    
-    if ((dots->x < leftUp.x && dots-> y > leftUp.y && distLeftUp >= errRate && distLeftUp < 3 * errRate) || 
-        (dots->x < leftBot.x && dots-> y < leftBot.y && distLeftUp >= errRate && distLeftUp < 3 * errRate) ||
-        (dots->x > rightUp.x && dots-> y > rightUp.y && distLeftUp >= errRate && distLeftUp < 3 * errRate) ||
-        (dots->x > leftBot.x && dots-> y < leftBot.y && distLeftUp >= errRate && distLeftUp < 3 * errRate) ||
-        (dots->x > leftLimit && dots->x < leftLimit + 2 * errRate) || 
-        (dots->x < rightLimit && dots->x < rightLimit - 2 * errRate) || 
-        (dots->y > botLimit && dots->y > botLimit + 2 * errRate) || 
-        (dots->y < topLimit && dots->y < topLimit - 2 * errRate)){
+    if ((distLeftUp <= errRate) || 
+        (distLeftBot <= errRate) ||
+        (distRightUp <= errRate) ||
+        (distRightBot <= errRate) ||
+        (dots->y > leftBot.y && dots->y < leftUp.y && dots->x > leftLimit && dots->x < leftLimit + 2 * errRate) || 
+        (dots->y > rightBot.y && dots->y < rightUp.y && dots->x < rightLimit && dots->x > rightLimit - 2 * errRate) || 
+        (dots->x < rightBot.x && dots->x > leftBot.x && dots->y > botLimit && dots->y < botLimit - 2 * errRate) || 
+        (dots->x < rightUp.x && dots->x > leftUp.x && dots->y < topLimit && dots->y > topLimit + 2 * errRate)){
         std::cout << "Point on contour\n";
     } else {
         std::cout << "Point not on contour\n";
--- practice0/point.cpp
+++ practice0/point.cpp
@@ -9,18 +9,18 @@ const float pi = 3.14;
 int main(){
     int opt = 0;
     bool flag = true;
-    std::cout << "circle = 1\nsquare = 2\n\n";
+    std::cout << "circle = 1\nsquare = 2\n";
     Dots dot;
     Circle firstCircle;
     Square firstSquare;
     Circle secondCircle;
     Square secondSquare;
     while (flag){
-        std::cin >> opt; 
+        std::cin >> opt;
         switch (opt) {
         case 1:
             //fill
-            std::cout << "write X, Y and radius\n\n";
+            std::cout << "\nwrite X, Y and radius\n";
             std::cin >> firstCircle.center.x >> firstCircle.center.y >> 
                 firstCircle.radius;
             firstCircle.len = 2 * pi * firstCircle.radius;
@@ -28,53 +28,51 @@ int main(){
                 firstCircle.radius;
             
             //print 
-            std::cout << "circle length: " << firstCircle.len << std::endl <<
+            std::cout << "\ncircle length: " << firstCircle.len << std::endl <<
                 "the area of the circle: " << firstCircle.square << std::endl;
             flag = false;
             break;
         case 2:
             //fill
-            std::cout << "write X, Y and side length\n\n";
+            std::cout << "\nwrite X, Y and side length\n";
             std::cin >> firstSquare.leftUp.x >> firstSquare.leftUp.y >> 
                 firstSquare.side;
             firstSquare.square = firstSquare.side * firstSquare.side;
             firstSquare.perimeter = 4 * firstSquare.side;
 
             //print
-            std::cout << "square area: " << firstSquare.square << std::endl <<
+            std::cout << "\nsquare area: " << firstSquare.square << std::endl <<
                 "the perimeter of the square: " << firstSquare.perimeter << std::endl;
             flag = false;
             break;
         default:
-            std::cout << "oops, something went wrong\ncircle = 1\nsquare = 2\n\n";
-            std::cin >> opt;
+            std::cout << "\noops, something went wrong\ncircle = 1\nsquare = 2\n";
             break;
         }
     }
     int optD = 0;
     flag = true;
-        std::cout << "check point = 1\ncheck circle = 2\ncheak square = 3\n\n";
-        std::cin >> optD;
+    std::cout << "\ncheck point = 1\ncheck circle = 2\ncheak square = 3\n";
     while (flag){
+        std::cin >> optD;
         switch (optD){
         case 1:
-            std::cout << "write X and Y\n\n";
+            std::cout << "\nwrite X and Y\n";
             std::cin >> dot.x >> dot.y;
             flag = false;
             break;
         case 2:
-            std::cout << "write X, Y and radius\n\n";
+            std::cout << "\nwrite X, Y and radius\n";
             std::cin >> secondCircle.center.x >> secondCircle.center.y >> secondCircle.radius;
             flag = false;
             break;
         case 3:
-            std::cout << "write X, Y and side length\n\n";
+            std::cout << "\nwrite X, Y and side length\n";
             std::cin >> secondSquare.leftUp.x >> secondSquare.leftUp.y >> secondSquare.side;
             flag = false;
             break;
         default:
-            std::cout << "oops, something went wrong\ncheck point = 1\ncheck circle = 2\ncheak square = 3\n\n";
-            std::cin >> optD;
+            std::cout << "\noops, something went wrong\ncheck point = 1\ncheck circle = 2\ncheak square = 3\n";
             break;
         }
     }
