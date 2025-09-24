1a920aff539709e1f0b82a46dfc0a60673d7dbf5
NeSustat <nesustat0v@icloud.com>
2025-09-24 23:44:16 +0300

    начало pz 2

---
+++ practice1/main.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <iostream>
+
+class arr {
+    int i;
+    void set(int n){
+        int curArr[i];
+        for (int j = 0; j < n; j++){
+            std::cin >> curArr[j];
+        }
+    }
+};
+
+int main(){
+    int a;
+    std::cout << ""; 
+}
